469a1ec60c1ed0c79f5d7ff520fd0d62e322cb66
maratonaucsal <70966804+maratonaucsal@users.noreply.github.com>
2020-09-08 13:22:40 -0300

    Add files via upload

---
+++ Java/2018 - 1ª Fase/DesvendandoMontyHall.java
@@ -0,0 +1,22 @@
+package sbc.facil;
+
+import java.util.Scanner;
+
+public class DesvendandoMontyHall {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int aux = 0, n = scan.nextInt();
+		int p;
+		for (int i = 0; i < n; i++) {
+			p = scan.nextInt();
+			if (p != 1) {
+				aux++;
+			}
+		}
+		System.out.println(aux);
+
+	}
+
+}
