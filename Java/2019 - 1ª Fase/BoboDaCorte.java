e10386a97e3ace81d7b2668dad80c82e47290401
Joaldo Tavares <54106303+joaldotavares@users.noreply.github.com>
2020-09-09 20:33:59 -0300

    Add files via upload

---
+++ Java/2019 - 1ª Fase/BoboDaCorte.java
@@ -0,0 +1,33 @@
+package sbc.facil;
+
+import java.util.Scanner;
+
+public class BoboDaCorte {
+	
+	private static Scanner scan;
+	
+	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		int vet[] = new int[n];
+		int aux = 0;
+		boolean v = true;
+		for (int i = 0; i < n; i++) {
+			vet[i] = scan.nextInt();
+		}
+		aux = vet[0];
+		
+		for (int i = 0; i < n; i++) {
+			if(aux < vet[i]) {
+				v = false;
+			}
+		}
+		if(v) {
+			System.out.println("S");
+		}else {
+			System.out.println("N");
+		}
+	}
+
+}
