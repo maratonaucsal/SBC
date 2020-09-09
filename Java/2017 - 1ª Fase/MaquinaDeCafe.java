188577a47e5bc024057f689eb1a5c13ef34baab2
Joaldo Tavares <54106303+joaldotavares@users.noreply.github.com>
2020-09-09 20:12:34 -0300

    Add files via upload

---
+++ Java/2017 - 1ª Fase/MaquinaDeCafe.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class MaquinaDeCafe {
+
+	private static Scanner scan;
+
+	public static void main(String[] args) {
+		int a1 = 0, a2 = 0, a3 = 0;
+		List<Integer> lista = new ArrayList<Integer>();
+		scan = new Scanner(System.in);
+		
+		a1 = scan.nextInt();
+		a2 = scan.nextInt();
+		a3 = scan.nextInt();
+		
+		lista.add((a2 * 2) + (a3 * 4));
+		lista.add((a1 * 2) + (a3 * 2));
+		lista.add((a1 * 4) + (a2 * 2));
+		
+		Collections.sort(lista);
+		
+		System.out.print(lista.get(0));
+	}
+
+}
